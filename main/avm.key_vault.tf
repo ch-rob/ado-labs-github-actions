e47df3e5f63284d182c33185a734064ea89c6605
Chad Voelker <chad.voelker@microsoft.com>
2025-04-24 08:30:02 -0500

    KeyVault Name Change

--- main/avm.key_vault.tf
+++ main/avm.key_vault.tf
@@ -17,17 +17,6 @@ module "key_vault" {
     }
   }
 
-#  role_assignments = {
-#    deployment_user_secrets = {
-#      role_definition_id_or_name = "Key Vault Administrator"
-#      principal_id               = data.azurerm_client_config.current.object_id
-#    }
-#    customer_managed_key = {
-#      role_definition_id_or_name = "Key Vault Crypto Service Encryption User"
-#      principal_id               = module.user_assigned_managed_identity.principal_id
-#    }
-#  }
-
   wait_for_rbac_before_key_operations = {
     create = "60s"
   }
